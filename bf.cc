a52a872e13701e3bbe30c8b8b320aab048423ad7
Andrew Zyabin <zyabin1015@users.noreply.github.com>
2015-01-07 19:19:21 +0400

    Import the `bf` helper program
    from https://ru.wikipedia.org/wiki/Brainfuck

---
+++ bf.cc
@@ -0,0 +1,68 @@
+/**
+ * A Brainfucj interpreter in C++. 
+ * Taken from the russian Wikipedia <https://ru.wikipedia.org/wiki/Brainfuck>. 
+ */
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+ 
+using namespace std;
+ 
+static char cpu[30000];
+ 
+int main(int argc, char **argv)
+{
+ 
+    vector<char> acc;
+    char ch;
+    ifstream infile(argv[1]);
+    while(infile)
+    {
+        infile.get(ch);
+        acc.push_back(ch);
+    }
+    infile.close();
+    unsigned int j = 0;
+    int brc = 0;
+    for(int i = 0; i < acc.size(); ++i)
+    {
+        if(acc[i] == '>') j++;
+        if(acc[i] == '<') j--;
+        if(acc[i] == '+') cpu[j]++;
+        if(acc[i] == '-') cpu[j]--;
+        if(acc[i] == '.') cout << cpu[j];
+        if(acc[i] == ',') cin >> cpu[j];
+        if(acc[i] == '[')
+        {
+            if(!cpu[j])
+            {
+                ++brc;
+                while(brc)
+                {
+                    ++i;
+                    if (acc[i] == '[') ++brc;
+                    if (acc[i] == ']') --brc;
+                }
+            }else continue;
+        }
+        else if(acc[i] == ']')
+        {
+            if(!cpu[j])
+            {
+                continue;
+            }
+            else
+            {
+                if(acc[i] == ']') brc++;
+                while(brc)
+                {
+                    --i;
+                    if(acc[i] == '[') brc--;
+                    if(acc[i] == ']') brc++;
+                }
+                --i;
+            }
+        }
+    }
+}
